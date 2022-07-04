9878ae455d82b73e0df09b5e3ff1988d5e26d1c4
Cesar <cesr.sntn@gmail.com>
2022-07-03 23:43:48 -0400

    completed project

--- javascript.js
+++ javascript.js
@@ -1,14 +1,105 @@
-const container = document.getElementById("container");
+const container = document.querySelector('.container')
+const btnBlack = document.createElement('button')
+const btnRgb = document.createElement('button')
+const btnGray = document.createElement('button')
+const btnSize = document.createElement('button')
+const section = document.querySelector('.section');
+const buttonsContainer = document.querySelector('.buttons');
 
-function makeRows(rows, cols) {
-  container.style.setProperty('--grid-rows', rows);
-  container.style.setProperty('--grid-cols', cols);
-  for (c = 0; c < (rows * cols); c++) {
-    let cell = document.createElement("div");
+window.onload = () => {
+    const boxs = container.querySelectorAll('.box')
+    boxs.forEach(box => box.addEventListener('mouseover', () => {
+        box.style.background = 'black'
+    }))
+}
+
+
+function creatDivs(col , rows) {
+    for(let i = 0;i < (col * rows); i++) {
+        const div = document.createElement('div') 
+        container.style.gridTemplateColumns = `repeat(${col}, 1fr)`;
+        container.style.gridTemplateRows = `repeat(${rows}, 1fr)`;
+        container.appendChild(div).classList.add('box')
+    }
+}
+creatDivs(16,16)
+
+function grayColor() { 
     
-    container.appendChild(cell).className = "grid-item";
-  };
-};
+    const boxs = container.querySelectorAll('.box')
+    btnGray.textContent = "GREY"
+    btnGray.addEventListener('click', () => {
+        boxs.forEach(box => box.addEventListener('mouseover', ()=> {
+            let RNum = Math.floor(Math.random() * 256);
+            let GrayScale = `rgb(${RNum},${RNum},${RNum})`
+            box.style.background = GrayScale;
+        }))
+    })
+    buttonsContainer.appendChild(btnGray).classList.add('btn')
+}
+grayColor()
 
-makeRows(10,10);
+
+function rgbColors() {
+    
+    const boxs = container.querySelectorAll('.box')
+    btnRgb.textContent = "RAINBOW"
+    btnRgb.addEventListener('click' ,() => {
+        boxs.forEach(box => box.addEventListener('mouseover', () => {
+            let R = Math.floor(Math.random() * 256);
+            let G = Math.floor(Math.random() * 256);
+            let B = Math.floor(Math.random() * 256);
+            const RGB = `rgb(${R},${G},${B})`;
+            box.style.background = RGB;
+        }))
+    })
+    buttonsContainer.appendChild(btnRgb).classList.add('btn')
+}
+rgbColors()
+
+function blackColor() {
+   
+    const boxs = container.querySelectorAll('.box')
+    btnBlack.textContent = 'BLACK';
+    btnBlack.addEventListener('click', function () {
+        boxs.forEach(box => box.addEventListener('mouseover', function() {
+            this.style.background = 'black'
+        }))
+    })
+    buttonsContainer.appendChild(btnBlack).classList.add('btn')
+}
+blackColor() 
+
+function reSet() {
+    const boxs = container.querySelectorAll('.box')
+    boxs.forEach(box => {
+        box.remove();
+    })
+}
+
+
+function reSize() {
+   
+    btnSize.textContent = 'GRID SIZE'
+    btnSize.addEventListener('click', () => {
+        let user = prompt("what will your grid size be?")
+        if(user === null || user < 1 || user>100){
+            reSet();
+            creatDivs(16,16);
+            grayColor();
+            rgbColors();
+            blackColor();
+            alert("Invalid entry. Must be between 1 and 100.");
+        
+        } else { 
+            reSet();
+            creatDivs(user,user);
+            grayColor();
+            rgbColors();
+            blackColor();
+        }
+    })
+    buttonsContainer.appendChild(btnSize).classList.add('btn')
+}
+reSize()
 
